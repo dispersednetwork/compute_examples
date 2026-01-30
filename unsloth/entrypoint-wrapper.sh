d5ed65b34b96b801b3cc4af8347194e8f27c5c30
Peter H. Ezetta <peter.ezetta@otoy.com>
2026-01-30 15:08:26 -0700

    simplifying things to just use unsloth user

--- unsloth/entrypoint-wrapper.sh
+++ unsloth/entrypoint-wrapper.sh
@@ -4,14 +4,7 @@ set -e
 # Optional: log so we know it ran
 echo "[zerworker] entrypoint wrapper starting"
 
-# SSH key for zeruser
-if [[ -n "${SSH_KEY}" ]]; then
-    sudo mkdir -p /opt/zerworker/.ssh
-    echo "${SSH_KEY}" | sudo tee /opt/zerworker/.ssh/authorized_keys > /dev/null
-    sudo chmod 700 /opt/zerworker/.ssh
-    sudo chmod 600 /opt/zerworker/.ssh/authorized_keys
-    sudo chown -R zeruser:zeruser /opt/zerworker/.ssh
-fi
+export SSH_KEY="${SSH_PUBKEY}"
 
 echo "[zerworker] handing off to unsloth entrypoint"
 
